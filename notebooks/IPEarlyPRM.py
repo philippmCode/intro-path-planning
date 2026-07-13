db76f930d0f59fb037670265da12d238c9dd423d
philippmCode <pdm_meyer@yahoo.com>
2026-07-13 12:05:54 +0200

    Add EarlyPRM class for enhanced path planning

---
+++ notebooks/IPEarlyPRM.py
@@ -0,0 +1,165 @@
+# coding: utf-8
+"""
+Task 4
+"""
+
+from IPPRMBase import PRMBase
+from scipy.spatial import cKDTree
+import networkx as nx
+import random
+import time
+
+from IPPerfMonitor import IPPerfMonitor
+
+class EarlyPRM(PRMBase):
+
+    def __init__(self, _collChecker):
+        super(EarlyPRM, self).__init__(_collChecker)
+        
+        self.graph = nx.Graph()
+        self.lastGeneratedNodeNumber = 0
+        self.collidingEdges = []
+        self.nonCollidingEdges =[]
+
+        self.stats = {
+            "point_in_collision_calls": 0,
+            "line_in_collision_calls": 0,
+            "removed_colliding_nodes": 0,
+            "removed_colliding_edges": 0,
+            "confirmed_free_edges": 0,
+            "planning_time_seconds": 0.0
+        }
+        
+    @IPPerfMonitor
+    def _buildRoadmap(self, numNodes, kNearest):
+        
+        # generate #numNodes nodes
+        addedNodes = []
+
+        for i in range(numNodes):
+            pos = self._getRandomPosition()
+
+            if not self._checkNodeForCollisionAndUpdate(pos):
+                self.graph.add_node(self.lastGeneratedNodeNumber, pos=pos)
+                addedNodes.append(self.lastGeneratedNodeNumber)
+                self.lastGeneratedNodeNumber += 1
+            else:
+                self.stats["removed_colliding_nodes"] += 1
+                
+
+        # for every node in graph find nearest neigbhours
+        posList = list(nx.get_node_attributes(self.graph,'pos').values())
+        #print posList
+        kdTree = cKDTree(posList)
+        
+        # to see when _buildRoadmap has to be called again
+        #print addedNodes
+        for node in addedNodes:
+        #for node in self.graph.nodes():
+        # Find set of candidates to connect to sorted by distance
+            result = kdTree.query(self.graph.nodes[node]['pos'],k=kNearest)
+            for data in result[1]:
+                c_node = [x for x, y in self.graph.nodes(data=True) if (y['pos']==posList[data])][0]
+                if node!=c_node:
+                    if (node, c_node) not in self.collidingEdges:
+                        self.graph.add_edge(node,c_node)
+                    else:
+                        continue
+                        #print "not adding already checked colliding edge"
+
+    @IPPerfMonitor
+    def _checkNodeForCollisionAndUpdate(self, pos):
+
+        self.stats["point_in_collision_calls"] += 1
+        if self._collisionChecker.pointInCollision(pos):
+            return True
+            #print "Colliding Node"
+        return False
+    
+    @IPPerfMonitor
+    def _checkEdgesForCollisionAndUpdate(self,path):
+        
+        # check all path segments
+        for elem in zip(path,path[1:]):
+            #print elem
+            x = elem[0]
+            y = elem[1]
+            self.stats["line_in_collision_calls"] += 1
+            if self._collisionChecker.lineInCollision(self.graph.nodes()[x]['pos'],self.graph.nodes()[y]['pos']):
+                self.graph.remove_edge(x,y)
+                self.collidingEdges.append((x,y))
+                self.stats["removed_colliding_edges"] += 1
+                return True
+            else:
+                self.nonCollidingEdges.append((x,y))
+                self.stats["confirmed_free_edges"] += 1
+                                                                            
+        return False
+        
+    @IPPerfMonitor   
+    def planPath(self, startList, goalList, config):
+        """
+        
+        Args:
+            startList (array): start position in planning space
+            goalList (array) : goal position in planning space
+            config (dict): dictionary with the needed information about the configuration options
+            
+        Example:
+        
+            config["initialRoadmapSize"] = 40 # number of nodes of first roadmap
+            config["updateRoadmapSize"]  = 20 # number of nodes to add if there is no connection from start to end
+            config["kNearest"] = 5 # number of nodes to connect to during setup
+            config["maxIterations"] = 40 # number of iterations trying to refine the roadmap
+            
+        """
+
+        start_time = time.time()
+
+        # Reset tracking stats for a fresh planning run
+        self.stats = {
+            "point_in_collision_calls": 0,
+            "line_in_collision_calls": 0,
+            "removed_colliding_nodes": 0,
+            "removed_colliding_edges": 0,
+            "confirmed_free_edges": 0,
+            "planning_time_seconds": 0.0
+        }
+
+        # 0. reset
+        self.graph.clear()
+        self.lastGeneratedNodeNumber = 0
+        self.collidingEdges = []
+        
+        # 1. check start and goal whether collision free (s. BaseClass)
+        checkedStartList, checkedGoalList = self._checkStartGoal(startList,goalList)
+        
+        # 2. add start and goal to graph
+        self.graph.add_node("start", pos=checkedStartList[0])
+        self.graph.add_node("goal", pos=checkedGoalList[0])
+        
+        # 3. build initial roadmap
+        self._buildRoadmap(config["initialRoadmapSize"], config["kNearest"])
+        
+        maxTry = 0
+        while maxTry < config["maxIterations"]: 
+            try:
+                path = nx.shortest_path(self.graph,"start","goal")
+            except:
+                self._buildRoadmap(config["updateRoadmapSize"], config["kNearest"])
+                maxTry += 1
+                continue
+              
+            if self._checkEdgesForCollisionAndUpdate(path):
+                continue
+            else:
+                #print "Found solution"
+                return path
+            
+
+        end_time = time.time()
+        self.stats["planning_time_seconds"] = end_time - start_time
+
+        return []
+
+    
